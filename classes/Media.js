e94013785687e29b3ba6903901035bfc509233e7
littlegaz <gareth.t.horne@hotmail.co.uk>
2024-05-08 12:30:55 +0100

    media done

--- classes/Media.js
+++ classes/Media.js
@@ -1,4 +1,22 @@
 // create your Media class:
 
+class Media{
+    static totalMediaCount = 0
+    constructor(title, year, genre){
+        this.title = title
+        this.year = year
+        this.genre = genre
+        Media.totalMediaCount++
+    }
+
+    summary(){
+        return `Title: ${this.title}, Year: ${this.year}, Genre: ${this.genre},`
+    }
+
+    static getTotalMediaCount(){
+        return Media.totalMediaCount
+    }
+}
+
 // uncomment below to export it:
 module.exports = Media;
